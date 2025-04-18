ad55600b66df72ae8e49a53e8465e5d631c2bbe6
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-18 18:37:17 -0500

    Update settings.py

--- nextupgaming/settings.py
+++ nextupgaming/settings.py
@@ -1,2 +1,8 @@
 # settings placeholder
 WSGI_APPLICATION = 'nextupgaming.wsgi.application'
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
