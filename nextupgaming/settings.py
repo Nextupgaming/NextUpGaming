a4d395391ae3be986f2ad30b8c4f870899c8cbba
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-18 18:48:22 -0500

    Update settings.py

--- nextupgaming/settings.py
+++ nextupgaming/settings.py
@@ -1,9 +1,14 @@
 import dj_database_url
 import os
-WSGI_APPLICATION = 'nextupgaming.wsgi.application'
+
+raw_db_url = os.environ.get("DATABASE_URL")
+
+if isinstance(raw_db_url, bytes):  # Decode if needed
+    raw_db_url = raw_db_url.decode()
+
 DATABASES = {
     'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
+        raw_db_url,
         conn_max_age=600,
         ssl_require=True
     )
