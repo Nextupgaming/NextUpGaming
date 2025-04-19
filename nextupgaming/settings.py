4e61e61ef5721844bfe96367da3dbae13c513dac
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-19 11:36:49 -0500

    Add files via upload

--- nextupgaming/settings.py
+++ nextupgaming/settings.py
@@ -1,10 +1,10 @@
-
 import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'your-secret-key'
+SECRET_KEY = 'django-insecure-change-this-in-production'
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
@@ -16,8 +16,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nextupgaming',
+    'users',
     'shop',
+    'teams',
+    'tournaments',
 ]
 
 MIDDLEWARE = [
@@ -27,6 +29,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'nextupgaming.urls'
@@ -56,14 +59,22 @@ DATABASES = {
     }
 }
 
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/profile/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
--- nextupgaming/urls.py
+++ nextupgaming/urls.py
@@ -1,12 +1,11 @@
-
 from django.contrib import admin
 from django.urls import path, include
-from .views import signup_view, profile_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/signup/', signup_view, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('profile/', profile_view, name='profile'),
-    'shop',
+    path('accounts/', include('users.urls')),
+    path('shop/', include('shop.urls')),
+    path('teams/', include('teams.urls')),
+    path('tournaments/', include('tournaments.urls')),
 ]
