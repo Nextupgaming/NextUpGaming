a65da28faa311e6c999e7a0fc4749d1f78da5754
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-18 19:42:06 -0500

    Update urls.py

--- nextupgaming/urls.py
+++ nextupgaming/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
+from django.contrib.auth import views as auth_views
+from nextupgaming import views
 
 def home(request):
-    return HttpResponse("🚀 NextUpGaming is LIVE!")
+    return HttpResponse("✅ NextUpGaming is LIVE!")
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
