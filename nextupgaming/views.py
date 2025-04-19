868d99cebe4880664859f6fb1157fbdc241cf472
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-18 21:37:39 -0500

    Update views.py

--- nextupgaming/views.py
+++ nextupgaming/views.py
@@ -10,3 +10,16 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('login')  # or redirect to dashboard
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
