47677b58d98865bff4c4b1f1b56a9e26515f4f1c
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-19 12:16:02 -0500

    Add files via upload

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+@login_required
+def profile_view(request):
+    return render(request, 'profile.html')
