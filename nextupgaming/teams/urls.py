e547bd97faa5ae28a4d3f3eb751603da50884fb9
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-19 11:49:45 -0500

    Delete nextupgaming/teams directory

--- nextupgaming/teams/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.team_list, name='team_list'),
-    path('create/', views.team_create, name='team_create'),
-    path('<int:team_id>/', views.team_detail, name='team_detail'),
-]
