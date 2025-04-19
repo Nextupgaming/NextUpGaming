79e1d6c5b3438f37b5f3c4419d15e57e8759affd
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-18 19:58:24 -0500

    Update settings.py

--- nextupgaming/settings.py
+++ nextupgaming/settings.py
@@ -5,12 +5,21 @@ import os
 
 import os
 
-TEMPLATES = [
+   TEMPLATES = [
     {
-        
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'nextupgaming' / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
+]
 ]
 
 INSTALLED_APPS = [
