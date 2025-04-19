3ed325efb2be5d749327db3f1ee7ce628953afb3
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-19 13:53:38 -0500

    Add files via upload

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Item
 
-def item_detail(request, item_id):
-    item = get_object_or_404(Item, id=item_id)
-    return render(request, 'shop/item_detail.html', {'item': item})
-
 def shop_home(request):
-    return render(request, 'shop/shop_home.html')
+    items = Item.objects.all()
+    return render(request, 'shop/shop_home.html', {'items': items})
