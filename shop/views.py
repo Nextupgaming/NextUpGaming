5edf19d85394fb55f70a00297dd48803025da781
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-19 10:30:57 -0500

    Add files via upload

--- shop/views.py
+++ shop/views.py
@@ -11,8 +11,20 @@ def shop_view(request):
 @login_required
 def purchase_view(request, item_id):
     item = get_object_or_404(Item, id=item_id)
-    if request.user.userprofile.age < 18 and item.age_restricted:
+    profile = request.user.userprofile
+
+    # Age check
+    if profile.age < 18 and item.age_restricted:
         return render(request, 'shop/restricted.html')
-    # Assume user has enough coins - logic to be expanded
+
+    # Coin balance check
+    if profile.coin_balance < item.price_in_coins:
+        return render(request, 'shop/insufficient_coins.html', {'item': item})
+
+    # Deduct coins and save
+    profile.coin_balance -= item.price_in_coins
+    profile.save()
+
+    # Create order
     Order.objects.create(user=request.user, item=item)
     return render(request, 'shop/confirmation.html', {'item': item})
