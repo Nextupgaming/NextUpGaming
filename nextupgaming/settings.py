2cd37f73ad7d5784b1c78a8d92ce6a290411633b
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-18 20:20:02 -0500

    Update settings.py

--- nextupgaming/settings.py
+++ nextupgaming/settings.py
@@ -34,12 +34,14 @@ ROOT_URLCONF = 'nextupgaming.urls'
 WSGI_APPLICATION = 'nextupgaming.wsgi.application'
 if isinstance(raw_db_url, bytes):  # Decode if needed
     raw_db_url = raw_db_url.decode()
-DATABASES = {
+
+    DATABASES = {
     'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL", "").decode("utf-8") if isinstance(os.environ.get("DATABASE_URL"), bytes) else str(os.environ.get("DATABASE_URL")),
+        os.environ.get("DATABASE_URL", ""),
         conn_max_age=600,
         ssl_require=True
     )
+}
 
     LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
