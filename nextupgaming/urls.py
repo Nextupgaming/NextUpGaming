17f728e99e105edcb2f4ac8a758336a5f37262ac
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-19 13:57:25 -0500

    Update urls.py

--- nextupgaming/urls.py
+++ nextupgaming/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from shop.views import shop_home
+from django.urls import path, include
+from users import views as user_views  # if you created a custom signup view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', shop_home, name='shop_home'),
+    path('accounts/', include('django.contrib.auth.urls')),  # For login/logout
+    path('accounts/signup/', user_views.signup_view, name='signup'),  # Your custom signup
+    path('shop/', include('shop.urls')),
 ]
