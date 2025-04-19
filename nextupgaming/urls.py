5c1e399eb2ebe4185621a2f333d16b1e961415e2
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-19 12:34:25 -0500

    Add files via upload

--- nextupgaming/urls.py
+++ nextupgaming/urls.py
@@ -5,8 +5,6 @@ from shop.views import shop_home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', shop_home, name='shop_home'),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('users.urls')),
     path('shop/', include('shop.urls')),
-    path('tournaments/', include('tournaments.urls')),
+    path('accounts/', include('users.urls')),
 ]
