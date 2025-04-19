8030e4d24b4e31e68ef495211dc1a63c40a2a917
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-19 12:11:38 -0500

    Add files via upload

--- shop/views.py
+++ shop/views.py
@@ -1,26 +1,4 @@
-from django.shortcuts import render, redirect
-from .models import Item, Order
-from django.contrib.auth.decorators import login_required
-from .forms import PurchaseForm
+from django.shortcuts import render
 
-@login_required
 def shop_home(request):
-    items = Item.objects.all()
-    return render(request, "shop/shop_home.html", {"items": items})
-
-@login_required
-def item_detail(request, item_id):
-    item = Item.objects.get(id=item_id)
-    if request.method == "POST":
-        form = PurchaseForm(request.POST)
-        if form.is_valid():
-            Order.objects.create(user=request.user, item=item, quantity=form.cleaned_data['quantity'])
-            return redirect("shop_home")
-    else:
-        form = PurchaseForm()
-    return render(request, "shop/item_detail.html", {"item": item, "form": form})
-
-@login_required
-def order_history(request):
-    orders = Order.objects.filter(user=request.user)
-    return render(request, "shop/order_history.html", {"orders": orders})
+    return render(request, 'shop/home.html')
