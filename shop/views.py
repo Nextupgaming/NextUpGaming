4302a53ddd08f75f3dde5a6019810f00302cdbd4
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-19 13:15:50 -0500

    Add files via upload

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,5 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from .models import Order
 
-@login_required
-def order_history(request):
-    orders = Order.objects.filter(user=request.user).order_by('-created_at')
-    return render(request, 'shop/order_history.html', {'orders': orders})
+from django.shortcuts import render
+
+def shop_home(request):
+    return render(request, 'shop/home.html')
