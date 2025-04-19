ccdb03bccd768517de3129e37963e16603d6dadc
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-19 13:51:13 -0500

    Add files via upload

--- nextupgaming/urls.py
+++ nextupgaming/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from shop.views import shop_home
-
-from django.shortcuts import render
-
-def shop_home(request):
-    return render(request, 'shop/shop_home.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', shop_home, name='shop_home'),
-    path('shop/', include('shop.urls')),
-    path('accounts/', include('users.urls')),
+    path('shop/', shop_home, name='shop_home'),
 ]
