cae3543b927c5bd764631d92dcdb2bf002ff2ce7
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-18 23:32:11 -0500

    Update urls.py

--- nextupgaming/urls.py
+++ nextupgaming/urls.py
@@ -23,6 +23,14 @@ from django.contrib import admin
 from django.urls import path, include
 from nextupgaming.views import signup_view  # 👈 ADD THIS
 
+from nextupgaming.views import signup_view, profile_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/signup/', signup_view, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('profile/', profile_view, name='profile'),  # 👈 ADD THIS
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/signup/', signup_view, name='signup'),  # 👈 ADD THIS
