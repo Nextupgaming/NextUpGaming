f1c615483ae5022eac17335c29f0dc4eb7e62a2f
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-19 13:24:18 -0500

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,6 @@ from .models import Item
 def item_detail(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     return render(request, 'shop/item_detail.html', {'item': item})
+
+def shop_home(request):
+    return render(request, 'shop/shop_home.html')
