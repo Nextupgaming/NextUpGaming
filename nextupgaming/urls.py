c478f52bda24492fc73fdd4379bc824963cc6d48
Nextupgaming <nextupgaming.gg@gmail.com>
2025-04-19 13:28:58 -0500

    Update urls.py

--- nextupgaming/urls.py
+++ nextupgaming/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from shop.views import shop_home
 
+from django.shortcuts import render
+
+def shop_home(request):
+    return render(request, 'shop/shop_home.html')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', shop_home, name='shop_home'),
